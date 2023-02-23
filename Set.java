c9e634c93018d9f86886bdd5634624e7cbae6f32
JonathanP34 <90928826+JonathanP34@users.noreply.github.com>
2023-02-22 22:08:57 -0500

    Update Set.java

--- Set.java
+++ Set.java
@@ -2,26 +2,15 @@ public class Set<T> {
   private LinearNode<T> setStart; //The front of the linked list
 
   public Set() {
-    setStart = null;
+    setStart = new LinearNode<T>(); //Setstart is a node that only points, no value
   }
 
   public void add(T element) {
     //Creating a new node called newElem that must be added to the linked list
     LinearNode<T> newElem = new LinearNode<T>(element); 
-    
-    if(setStart == null) { //If there is no element in the linked list
-      setStart.setNext(newElem); //Set newElem as the first node
-    }
-    else {
-      LinearNode<T> curr = setStart; //Setting a variable to loop though linked list
-      while (curr.getNext() != null) {
-        curr = curr.getNext(); //Moving the counter up 1
-        if (curr.getNext() == null) { //If the value after curr is null
-          curr.getNext().setNext(newElem); //Set that value to newElem
-          break; //Break as we have already added our element to the linked list
-        }
-      }
-    } 
+
+    newElem.setNext(setStart.getNext());
+    setStart.setNext(newElem);
   }
 
 
@@ -40,7 +29,10 @@ public class Set<T> {
   public T getElement(int i) {
     LinearNode<T> curr = setStart;
 
-    for (int j = 0; j < i; j++) { //Looping through the linked list i times
+    //Changing i to properly represent the index
+    i = getLength() + 1 - i;
+    
+    for (int j = 0; j  < i; j++) { //Looping through the linked list i times
       curr = curr.getNext();
     }
 
@@ -52,10 +44,9 @@ public class Set<T> {
     while (curr.getNext() != null) { //Looping through the linked list
       curr = curr.getNext(); //Putting it the beginning because setStart has no element
       //If the value at curr == element or .equals element then return true
-      if (curr.getElement() == element || curr.getElement().equals(element)) { //Potential erorr here
+      if (curr.getElement().equals(element)) { //Potential erorr here
         return true;
-      }
-      
+      }  
     }
     //If not there, return false
     return false;   
@@ -69,6 +60,7 @@ public class Set<T> {
       curr = curr.getNext();
       s += curr.getElement();
       s += " ";
+      
     }
 
     return s; 
