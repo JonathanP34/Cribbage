e8ab0f08c6a2efbd31ba9563e3f58bd05f33c2ea
JonathanP34 <90928826+JonathanP34@users.noreply.github.com>
2023-02-22 19:57:10 -0500

    Update Set.java

--- Set.java
+++ Set.java
@@ -26,7 +26,52 @@ public class Set<T> {
 
 
   public int getLength() {
-    
+    LinearNode<T> curr = setStart; //Setting curr to setStart
+    int ct = 0; //Making a counter
+
+    while (curr.getNext() != null) {//Looping through the linked list
+      ct += 1; //Inceasing the counter
+      curr = curr.getNext(); 
+    }
+    return ct;  
+  }
+
+
+  public T getElement(int i) {
+    LinearNode<T> curr = setStart;
+
+    for (int j = 0; j < i; j++) { //Looping through the linked list i times
+      curr = curr.getNext();
+    }
+
+    return curr.getElement();
+  } 
+
+  public boolean contains(T element) {
+    LinearNode<T> curr = setStart;
+    while (curr.getNext() != null) { //Looping through the linked list
+      curr = curr.getNext(); //Putting it the beginning because setStart has no element
+      //If the value at curr == element or .equals element then return true
+      if (curr.getElement() == element || curr.getElement().equals(element)) { //Potential erorr here
+        return true;
+      }
+      
+    }
+    //If not there, return false
+    return false;   
+  }
+
+
+  public String toString() {
+    LinearNode<T> curr = setStart;
+    String s = ""; //Building on the string
+    while (curr.getNext() != null) { //Looping through the list
+      curr = curr.getNext();
+      s += curr.getElement();
+      s += " ";
+    }
+
+    return s; 
   }
   
 }
